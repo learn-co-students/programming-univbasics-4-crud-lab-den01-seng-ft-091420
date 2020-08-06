550d1148b28c33271a049a748ad7446ff76a7234
generic-state-5961 <ericjchristine@gmail.com>
2020-08-06 15:12:46 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["wow", "am", 2, "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["arrays" << "!"]
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+ create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  create_an_array[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  new_array = ["wow", "fun", "arrays!"]
+  return new_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  new_array = ["wow", "arrays!"]
+  new_array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  new_array = [1, 2, 3, 4, 5]
+  new_array[4] = "totally"
 end
